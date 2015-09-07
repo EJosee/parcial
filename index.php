3bfdc9a976aa2d6716bf5540ad42b0c72fd9b31e
EJosee <shadow_dragon-1@hotmail.com>
2015-09-07 16:13:39 -0600

    Actualizado-Erick

--- index.php
+++ index.php
@@ -140,7 +140,7 @@
 			
 			<div class="col-md-3 col-sm-6 hero-feature">
                 <div class="thumbnail">
-                    <img class="img-circle img-responsive img-center" src="imgs/p-narrow/02.jpg" alt="">
+                    <img class="img-circle img-responsive img-center" src="imgs/p-narrow/03.jpg" alt="">
                     <div class="caption">
                         <h3>Descripcion</h3>
                         <p>....</p>
@@ -201,15 +201,21 @@
 				<div class="modal-content">
 					<div class="modal-header">
 						<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-						<h4 class="modal-title">Modal title</h4>
+						<h4 class="modal-title">Titulo</h4>
 					</div>
-					<div class="modal-body">
-					<img class="img-circle img-responsive img-center" src="http://placehold.it/800x500" alt="">
-						<p>One fine body&hellip;</p>
-					</div>
+					<div class="row">
+							<div class="col-md-5">
+								<img class="img-responsive" src="imgs/calz/28.jpg" alt="">
+							</div>
+							<div class="col-md-5">
+								<strong><p>Detalles</p></strong>
+									<li class="glyphicon " aria-hidden="true"> </li>
+									<br/>
+							</div>
+						</div>
 					<div class="modal-footer">
 						<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-						<button type="button" class="btn btn-primary">Save changes</button>
+						<button type="button" class="btn btn-primary">Realizar Compra!</button>
 					</div>
 				</div><!-- /.modal-content -->
 			</div><!-- /.modal-dialog -->
@@ -220,14 +226,21 @@
 				<div class="modal-content">
 					<div class="modal-header">
 						<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-						<h4 class="modal-title">Modal title</h4>
+						<h4 class="modal-title">Titulo</h4>
 					</div>
-					<div class="modal-body">
-						<p>One fine body&hellip;</p>
-					</div>
+					<div class="row">
+							<div class="col-md-5">
+								<img class="img-responsive" src="imgs/calz/15.jpg" alt="">
+							</div>
+							<div class="col-md-5">
+								<strong><p>Detalles</p></strong>
+									<li class="glyphicon " aria-hidden="true"> </li>
+									<br/>
+							</div>
+						</div>
 					<div class="modal-footer">
 						<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-						<button type="button" class="btn btn-primary">Save changes</button>
+						<button type="button" class="btn btn-primary">Realizar Compra!</button>
 					</div>
 				</div><!-- /.modal-content -->
 			</div><!-- /.modal-dialog -->
@@ -238,14 +251,21 @@
 				<div class="modal-content">
 					<div class="modal-header">
 						<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-						<h4 class="modal-title">Modal title</h4>
+						<h4 class="modal-title">Titulo</h4>
 					</div>
-					<div class="modal-body">
-						<p>One fine body&hellip;</p>
-					</div>
+					<div class="row">
+							<div class="col-md-5">
+								<img class="img-responsive" src="imgs/calz/29.jpg" alt="">
+							</div>
+							<div class="col-md-5">
+								<strong><p>Detalles</p></strong>
+									<li class="glyphicon " aria-hidden="true"> </li>
+									<br/>
+							</div>
+						</div>
 					<div class="modal-footer">
 						<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-						<button type="button" class="btn btn-primary">Save changes</button>
+						<button type="button" class="btn btn-primary">Realizar Compra!</button>
 					</div>
 				</div><!-- /.modal-content -->
 			</div><!-- /.modal-dialog -->
@@ -256,14 +276,21 @@
 				<div class="modal-content">
 					<div class="modal-header">
 						<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-						<h4 class="modal-title">Modal title</h4>
+						<h4 class="modal-title">Titulo</h4>
 					</div>
-					<div class="modal-body">
-						<p>One fine body&hellip;</p>
-					</div>
+					<div class="row">
+							<div class="col-md-5">
+								<img class="img-responsive" src="imgs/p-narrow/01.jpg" alt="">
+							</div>
+							<div class="col-md-5">
+								<strong><p>Detalles</p></strong>
+									<li class="glyphicon " aria-hidden="true"> </li>
+									<br/>
+							</div>
+						</div>
 					<div class="modal-footer">
 						<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-						<button type="button" class="btn btn-primary">Save changes</button>
+						<button type="button" class="btn btn-primary">Realizar Compra!</button>
 					</div>
 				</div><!-- /.modal-content -->
 			</div><!-- /.modal-dialog -->
@@ -274,14 +301,21 @@
 				<div class="modal-content">
 					<div class="modal-header">
 						<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-						<h4 class="modal-title">Modal title</h4>
+						<h4 class="modal-title">Titulo</h4>
 					</div>
-					<div class="modal-body">
-						<p>One fine body&hellip;</p>
-					</div>
+					<div class="row">
+							<div class="col-md-5">
+								<img class="img-responsive" src="imgs/calz/10.jpg" alt="">
+							</div>
+							<div class="col-md-5">
+								<strong><p>Detalles</p></strong>
+									<li class="glyphicon " aria-hidden="true"> </li>
+									<br/>
+							</div>
+						</div>
 					<div class="modal-footer">
 						<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-						<button type="button" class="btn btn-primary">Save changes</button>
+						<button type="button" class="btn btn-primary">Realizar Compra!</button>
 					</div>
 				</div><!-- /.modal-content -->
 			</div><!-- /.modal-dialog -->
@@ -292,14 +326,21 @@
 				<div class="modal-content">
 					<div class="modal-header">
 						<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-						<h4 class="modal-title">Modal title</h4>
+						<h4 class="modal-title">Titulo</h4>
 					</div>
-					<div class="modal-body">
-						<p>One fine body&hellip;</p>
-					</div>
+					<div class="row">
+							<div class="col-md-5">
+								<img class="img-responsive" src="imgs/calz/23.jpg" alt="">
+							</div>
+							<div class="col-md-5">
+								<strong><p>Detalles</p></strong>
+									<li class="glyphicon " aria-hidden="true"> </li>
+									<br/>
+							</div>
+						</div>
 					<div class="modal-footer">
 						<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-						<button type="button" class="btn btn-primary">Save changes</button>
+						<button type="button" class="btn btn-primary">Realizar Compra!</button>
 					</div>
 				</div><!-- /.modal-content -->
 			</div><!-- /.modal-dialog -->
@@ -311,14 +352,21 @@
 				<div class="modal-content">
 					<div class="modal-header">
 						<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-						<h4 class="modal-title">Modal title</h4>
+						<h4 class="modal-title">Titulo</h4>
 					</div>
-					<div class="modal-body">
-						<p>One fine body&hellip;</p>
-					</div>
+					<div class="row">
+							<div class="col-md-5">
+								<img class="img-responsive" src="imgs/p-narrow/05.jpg" alt="">
+							</div>
+							<div class="col-md-5">
+								<strong><p>Detalles</p></strong>
+									<li class="glyphicon " aria-hidden="true"> </li>
+									<br/>
+							</div>
+						</div>
 					<div class="modal-footer">
 						<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-						<button type="button" class="btn btn-primary">Save changes</button>
+						<button type="button" class="btn btn-primary">Realizar Compra!</button>
 					</div>
 				</div><!-- /.modal-content -->
 			</div><!-- /.modal-dialog -->
@@ -330,14 +378,21 @@
 				<div class="modal-content">
 					<div class="modal-header">
 						<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-						<h4 class="modal-title">Modal title</h4>
+						<h4 class="modal-title">Titulo</h4>
 					</div>
-					<div class="modal-body">
-						<p>One fine body&hellip;</p>
-					</div>
+					<div class="row">
+							<div class="col-md-5">
+								<img class="img-responsive" src="imgs/p-narrow/03.jpg" alt="">
+							</div>
+							<div class="col-md-5">
+								<strong><p>Detalles</p></strong>
+									<li class="glyphicon " aria-hidden="true"> </li>
+									<br/>
+							</div>
+						</div>
 					<div class="modal-footer">
 						<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-						<button type="button" class="btn btn-primary">Save changes</button>
+						<button type="button" class="btn btn-primary">Realizar Compra!</button>
 					</div>
 				</div><!-- /.modal-content -->
 			</div><!-- /.modal-dialog -->
